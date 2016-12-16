4fb3006adf39bf5a0d5b31704a9616eac8e9d14c
ALI ALRAWI <ali.developer90@gmail.com>
2016-12-16 10:01:29 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction),  message: "%{value} is not a valid category" }
   validate :is_valid_clickbait?
   def is_valid_clickbait?
-    if CB.none? { |e| e.match(self.title) }
+    if CB.all? { |e| ! e.match(self.title) }
       errors.add(:title, "Invalid clickbait")
     end
 
